ad23f68093bca7cab03f6a6137f5546fae514420
GoudekettingRM <r.goudeketting@gmail.com>
2020-02-10 10:25:11 +0100

    add post endpoint

--- Image/router.js
+++ Image/router.js
@@ -3,6 +3,22 @@ const Image = require("./model");
 
 const router = new Router();
 
+router.post("/", async (req, res, next) => {
+  try {
+    const { url, title } = req.body;
+    if (!url) {
+      res
+        .status(400)
+        .send("You should provide a url for the image.")
+        .end();
+    }
+    const newImage = await Image.create({ url, title });
+    res.json(newImage).end();
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.get("/", async (req, res, next) => {
   try {
     const allImages = await Image.findAll();
