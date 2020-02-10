19ddf5d7774a3bc37b2d3e1c266d4ff4c6d0e1fb
GoudekettingRM <r.goudeketting@gmail.com>
2020-02-10 11:16:54 +0100

    fix login feature

--- auth/router.js
+++ auth/router.js
@@ -1,27 +1,41 @@
 const { Router } = require("express");
 const { toJWT, toData } = require("./jwt");
+const User = require("../User/model");
+const bcrypt = require("bcrypt");
 
 const router = new Router();
 
 router.post("/login", async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    if (!email) {
+    if (!email || !password) {
       res
         .status(400)
-        .send("No email provided")
+        .send({ message: "Please provide a valid email and password" })
         .end();
-    }
-    if (!password) {
-      res
-        .status(400)
-        .send("No password provided")
-        .end();
-    }
+    } else {
+      const user = await User.findOne({
+        where: {
+          email: email
+        }
+      });
+      console.log("in first else statement");
 
-    res.send({
-      jwt: toJWT({ userId: 1 })
-    });
+      if (!user) {
+        res
+          .status(404)
+          .send({ message: "No user found with this email" })
+          .end();
+      } else if (bcrypt.compareSync(password, user.password)) {
+        res.send({
+          jwt: toJWT({ userId: user.id })
+        });
+      } else {
+        res.status(400).send({
+          message: "Password was incorrect"
+        });
+      }
+    }
   } catch (error) {
     next(error);
   }
